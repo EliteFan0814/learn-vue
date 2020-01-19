4261be7956b233a7bb39db08987b0fe3e4ac921d
codebug <fan552426811@outlook.com>
2020-01-19 18:08:53 +0800

    all

---
+++ Child.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="child">
+    <input type="text" name="fname" v-model="objTest.name">
+    <input type="text" name="pname" v-model="normalTest">
+    <button @click="change">改变</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['objTest','normalTest'],
+  data() {
+    return {
+      fname: 'fpc'
+    }
+  },
+  methods: {
+    change(){
+      this.$emit('update:normalTest',this.normalTest)
+    }
+  },
+}
+</script>
+
+<style lang="css">
+  .child{
+    height: 100px;
+    border: 1px solid red;
+  }
+</style>
---
+++ app.vue
@@ -0,0 +1,34 @@
+<template>
+<!--  测试 .sync  -->
+  <div class="app">
+    {{parentNum.name}}--{{myName}}
+    <div class="child-wrapper">
+      <Child :objTest="parentNum" :normalTest.sync="myName"></Child>
+    </div>
+  </div>
+</template>
+
+<script>
+import Child from './Child.vue'
+export default {
+  components:{
+    Child
+  },
+  data(){
+    return{
+      parentNum:{
+        name:'fpx'
+      },
+      myName:'fpc'
+    }
+  }
+}
+</script>
+
+<style>
+  .app{
+    border: 1px solid red;
+    width: 200px;
+    height: 200px;
+  }
+</style>
